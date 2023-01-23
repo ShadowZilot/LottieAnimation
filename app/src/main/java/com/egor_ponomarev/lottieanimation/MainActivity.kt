02b63a78f59cde601dd5aceeeadcdbd80309b43c
ShadowZilot <e.ponomaryow2017@yandex.ru>
2023-01-23 22:48:33 +0600

    Added animation

--- app/src/main/java/com/egor_ponomarev/lottieanimation/MainActivity.kt
+++ app/src/main/java/com/egor_ponomarev/lottieanimation/MainActivity.kt
@@ -2,10 +2,23 @@ package com.egor_ponomarev.lottieanimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieDrawable
 
 class MainActivity : AppCompatActivity() {
+    private val mAnimationView by lazy {
+        findViewById<LottieAnimationView>(R.id.animationView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mAnimationView.setAnimation(R.raw.typing_animation)
+        mAnimationView.repeatCount = LottieDrawable.INFINITE
+        mAnimationView.playAnimation()
     }
 }
